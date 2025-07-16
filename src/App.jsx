3b699df8f30521e2eef9113106a27fe1bcebdf08
Elromayssae <elromayssaeramdani@gmail.com>
2025-07-16 17:04:48 +0100

    test

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Expertise from "./components/Expertise";
+import ContactSection from "./components/ContactSection";
+import TestimonialsSection from "./components/TestimonialsSection";
+import Footer from "./components/Footer";
+
+
+
+function App() {
+
+  return (
+    <div>
+     <Expertise/>
+      <ContactSection/>
+      <TestimonialsSection/>
+      <Footer/>
+ 
+    </div>
+  );
+}
+
+export default App;
+
+
