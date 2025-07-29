506ab2349c0b240a63e422a44896ade3813294d3
Elromayssae <elromayssaeramdani@gmail.com>
2025-07-29 13:35:01 +0100

    ajout composants dyali

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ function App() {
   return(
       <Router>
       <Routes>
-        {/* الصفحة الرئيسية هي Page1 */}
+        
         <Route path="/" element={<Page1 />} />
 
-        {/* صفحة 2 */}
+        
         <Route path="/page2" element={<Page2 />} />
       </Routes>
     </Router>
